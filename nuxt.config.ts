e43f4fc21ccec80008027727dfce172eb9f078e3
Jaime Aleman <jjaimealeman@gmail.com>
2025-07-01 23:35:16 -0600

    feat(styles): add main.css file and add to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-04-25',
+  compatibilityDate: "2025-04-25",
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   // Nuxt Modules
   // https://nuxt.com/modules
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/eslint'
-  ],
   modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui-pro"],
+  css: ["~/assets/css/main.css"],
   hub: {
     database: true,
     kv: true,
@@ -20,9 +17,10 @@ export default defineNuxtConfig({
   nitro: {
     experimental: {
       // Enable Server API documentation within NuxtHub
-      openAPI: true
-    }
+      openAPI: true,
+    },
   },
   // Development
   devtools: { enabled: true },
-})
+  debug: true
+});
